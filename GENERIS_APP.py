b2be9097bfc64e4c04d6ef40547157b95445ffec
ECASIC <92279500+ECASIC@users.noreply.github.com>
2021-10-10 17:20:54 -0700

    Add files via upload

---
+++ GENERIS_APP.py
@@ -0,0 +1,15 @@
+#GENERIS_APP
+# Las dos líneas siguientes son necesaias para hacer 
+# compatible la interfaz Tkinter con los programas basados 
+# en versiones anteriores a la 8.5, con las más recientes. 
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+
+
+from tkinter import *    # Carga módulo tk (widgets estándar)
+from tkinter import ttk  # Carga ttk (para widgets nuevos 8.5+)
+
+# Define la ventana principal de la aplicación
+ventana_principal = Tk()
+
