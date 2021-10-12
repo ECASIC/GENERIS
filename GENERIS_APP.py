7a3d1a5fd767756446f39e6e8b9239890cebb9da
ECASIC <eca02021999@gmail.com>
2021-10-11 18:28:52 -0700

    main

--- GENERIS_APP.py
+++ GENERIS_APP.py
@@ -2,18 +2,21 @@
 # Las dos líneas siguientes son necesarias para hacer 
 # compatible la interfaz Tkinter con los programas basados 
 # en versiones anteriores a la 8.5, con las más recientes. 
-from tkinter import *
 from tkinter import messagebox
-from tkinter import ttk
 from tkinter import *    # Carga módulo tk (widgets estándar)
-from tkinter import ttk  # Carga ttk (para widgets nuevos 8.5+)
-import tkinter as tk #en lugar de escibir tikinter siempre solo se pone tk
-app= tk.Tk() #Nombre del menu de app
-app.geometry("400x400") # Da geometria de la pantalla de la app cuando se abre
-app.configure(background="beige") #Da color al fondo de la app
-tk.Wm.wm_title(app,"Generis") #Titulo a app cuando se abre 
-Label(app, text="Nombre:").grid(pady=5, row=0, column=0)
-Button(app, text="Nombre:").grid(pady=5, row=0, column=1)
+from tkinter import ttk, font  # Carga ttk (para widgets nuevos 8.5+)
+import getpass
+import tkinter as tk #en lugar de escibir tkinter siempre solo se pone tk
+class app():
+    def __init__(self):
+        self.app = Tk()
+        self.app.title("Acceso") #Nombre del menu de app
+        self.app.resizable(0,0)
+        fuente = font.Font(weight='bold')# Da geometria de la pantalla de la app cuando se abre
+
+
+
+
 #tk.Button( 
  #   app,
  #   text="Informacion",
